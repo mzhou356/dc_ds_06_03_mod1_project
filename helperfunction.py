f59b0b7530b5bd2808e11f8cfe7ce9a152dfd8c9
Mindy Zhou <minzhou16@gmail.com>
2019-06-16 23:10:58 -0400

    modifiedipynb

--- helperfunction.py
+++ helperfunction.py
@@ -11,26 +11,25 @@ def split_rows(df, col1, col2, sep):
     series = [pd.Series(row[col1],row[col2].split(sep))
     for _,row in df.iterrows()]
     table = pd.concat(series).reset_index()
-    
     rename_columns(table, {0: col1, 'index': col2})
     return table
 
-def object_binary(df, col, cond):
-    df[col] = df[col].apply(lambda x: 1 if x == cond else 0)
-    return df 
+
+def object_binary(df, col, *args):
+    conds = [cond for cond in args]
+    new_col = df[col].apply(lambda x: 1 if x in conds else 0)
+    return new_col
     
 def drop_repeatrows(df, col):
     df.drop_duplicates(subset = col, inplace = True)
-    return df  
-
+  
 def drop_columns(df, *args):
     cols = [col for col in args]
     df.drop(columns = cols, inplace = True)
-    return df
 
 def rename_columns(df, dict_map):
     df.rename(columns = dict_map, inplace = True)
-    return df
+  
 
 def stringtonum(df, cols, chars):
     table = str.maketrans(dict.fromkeys(chars))
@@ -40,9 +39,25 @@ def stringtonum(df, cols, chars):
     else:
         converted = df.loc[:,cols].dropna().applymap(lambda x: int(x.translate(table)))
         df.loc[:,cols] = converted
-    return df 
 
 def merge_tables(df1,df2, common_col,how):
     merged = df1.merge(df2,on = common_col, how = how )
     return merged
+
+def drop_NA(df, cols):
+    df.dropna(subset = cols, inplace = True)
+    
+
+def create_masks(df, cols, conds, relations):
+    # relations a np numpy operator 
+    masks = []
+    for i,col in enumerate(cols):
+        mask = relations[i](df[col], conds[i])
+        masks.append(mask)
+    return masks        
+
+def create_list(df, col, sep):
+    new_col = df[col].apply(lambda x: x.split(sep))
+    return new_col
+    
     
